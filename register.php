7980766928e0a7f3b47ba44324cb8648ef80149f
dawoodumer42 <33564649+dawoodumer42@users.noreply.github.com>
2019-06-21 15:56:08 +0500

    Complete Front-End Validation Added
    Front-End Validation added for:
    Email
    Name
    Password
    Confirm Password
    Password Criteria

--- register.php
+++ register.php
@@ -9,11 +9,40 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
     <link rel="stylesheet" href="styles.css">
-    
+    <script type="text/javascript">
+
+        function checkPassword(str)
+        {
+            var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
+            return re.test(str);
+        }
+
+        function validate(form)
+        {
+            document.getElementById('error').innerHTML = '';
+            if(form.password.value != "" && form.password.value == form.confirm_password.value) {
+                if(!checkPassword(form.password.value)) {
+                    var paragraph = document.getElementById("error");
+                    var text = document.createTextNode("\n*Password should be of atleast 6 length, must contain atleast 1 number, 1 uppercase and 1 lowercase letter.");
+                    paragraph.appendChild(text);
+
+                    return false;
+                }
+            } else {
+                var paragraph = document.getElementById("error");
+                var text = document.createTextNode("\n*Passwords do not match.");
+                paragraph.appendChild(text);
+                
+                return false;
+            }   
+            return true;
+        }
+
+    </script>
 </head>
 <body>
     <div class="login-form">
-        <form action="register.php" method="post">
+        <form action="register.php" method="post" onsubmit="return validate(this);">
             <h2 class="text-center" >
                 Chat System                
             </h1>
@@ -35,6 +64,7 @@
             <div class="form-group">
                 <button type="submit" class="btn btn-primary btn-block">Register</button>
             </div>
+            <p class="text-danger text-red" id="error"> </p>
             <div class="clearfix">
                 <!-- <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label> -->
                 <!-- <a href="#" class="pull-right">Forgot Password?</a> -->
@@ -43,4 +73,5 @@
         <p class="text-center"><a href="login.php">Already Have an Account? Log In here.</a></p>
     </div>
 </body>
+
 </html>                                		                            
